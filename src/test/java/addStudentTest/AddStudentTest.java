c4b1f0f8d181871a3cb2e4ecd75225470d4a229e
DESKTOP-GCTJHCU\ratiu <ratiucosmina@gmail.com>
2019-04-01 10:02:42 +0300

    Corrected a test

--- src/test/java/addStudentTest/AddStudentTest.java
+++ src/test/java/addStudentTest/AddStudentTest.java
@@ -22,4 +22,106 @@ public class AddStudentTest  {
             assert false;
         }
     }
+
+    @Test
+    public void testAddStudentIdInvalid(){
+        StudentXMLRepo repo=new StudentXMLRepo(new StudentValidator(),"file.xml");
+
+        String id="";
+        Student student=new Student(id,"nume",10,"email","indrumator");
+        try {
+            repo.save(student);
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testAddStudentNumeValid(){
+        StudentXMLRepo repo=new StudentXMLRepo(new StudentValidator(),"file.xml");
+
+        String nume="nume";
+        Student student=new Student("id",nume,10,"email","indrumator");
+        try {
+            assert repo.findOne("id")==null;
+            repo.save(student);
+            assert repo.findOne("id")==student;
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    @Test
+    public void testAddStudentNumeInvalid(){
+        StudentXMLRepo repo=new StudentXMLRepo(new StudentValidator(),"file.xml");
+
+        String nume="";
+        Student student=new Student("id",nume,10,"email","indrumator");
+        try {
+            repo.save(student);
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testAddStudentGrupaValid(){
+        StudentXMLRepo repo=new StudentXMLRepo(new StudentValidator(),"file.xml");
+
+        int grupa=936;
+        Student student=new Student("id","nume",grupa,"email","indrumator");
+        try {
+            assert repo.findOne("id")==null;
+            repo.save(student);
+            assert repo.findOne("id")==student;
+        } catch (ValidatorException e) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void testAddStudentGrupaInvalid(){
+        StudentXMLRepo repo=new StudentXMLRepo(new StudentValidator(),"file.xml");
+
+        Student student=new Student("id","nume",0,"email","indrumator");
+        try {
+            repo.save(student);
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testAddStudentEmailValid(){
+        StudentXMLRepo repo=new StudentXMLRepo(new StudentValidator(),"file.xml");
+
+        Student student=new Student("id","nume",10,"email","indrumator");
+        try {
+            assert repo.findOne("id")==null;
+            repo.save(student);
+            assert repo.findOne("id")==student;
+        } catch (ValidatorException e) {
+            e.printStackTrace();
+            assert false;
+        }
+    }
+
+    @Test
+    public void testAddStudentEmailInvalid(){
+        StudentXMLRepo repo=new StudentXMLRepo(new StudentValidator(),"file.xml");
+
+        String nume="";
+        Student student=new Student("id","nume",10,"","indrumator");
+        try {
+            repo.save(student);
+            assert false;
+        } catch (ValidatorException e) {
+            assert true;
+        }
+    }
+
 }
